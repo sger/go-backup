5a6066db4f5a8bc0fb59f6ca7053161f3b37c596
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-31 17:00:48 +0300

    updates

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"log"
 
-	"github.com/matryer/filedb"
+	"github.com/boltdb/bolt"
 	"github.com/sger/podule"
 )
 
@@ -14,31 +17,109 @@ type path struct {
 }
 
 func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln(fatalErr)
+		}
+	}()
+	var (
+		interval = flag.Int("interval", 10, "interval between checks (seconds)")
+		archive  = flag.String("archive", "archive", "path to archive location")
+		dbpath   = flag.String("db", "./backupdata", "path to database directory")
+	)
+	flag.Parse()
+	fmt.Println(*dbpath)
+	fmt.Println(*archive)
+	fmt.Println(*interval)
+
 	m := &podule.Monitor{
-		Destination: "test/archive",
+		Destination: *archive,
 		Archiver:    podule.ZIP,
 		Paths:       make(map[string]string),
 	}
-
-	db, err := filedb.Dial("../../../backupdata")
+	//../backup/backupdata.db
+	db, err := bolt.Open(*dbpath, 0600, nil)
 	if err != nil {
-		fmt.Println("Database not found")
+		fatalErr = err
 		return
 	}
 	defer db.Close()
-	col, err := db.C("paths")
-	if err != nil {
-		//fmt.Println(err)
-	}
-	var path path
-	col.ForEach(func(_ int, data []byte) bool {
-		if err := json.Unmarshal(data, &path); err != nil {
-			return true
-		}
-		m.Paths[path.Path] = path.Hash
-		return false
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("paths"))
+		var path podule.Path
+		b.ForEach(func(k, v []byte) error {
+			fmt.Printf("key=%v, value=%s\n", k, v)
+			if err := json.Unmarshal(v, &path); err != nil {
+				fatalErr = err
+				return fatalErr
+			}
+			fmt.Println(path.Hash)
+			m.Paths[path.Path] = path.Hash
+			return nil
+		})
+		return nil
 	})
+
+	if fatalErr != nil {
+		return
+	}
+
 	if len(m.Paths) < 1 {
-		fmt.Println("no paths")
+		fatalErr = errors.New("no paths - use backup tool to add at least one")
+		return
+	}
+
+	check(m, db)
+	/*signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+		for {
+			select {
+			case <-time.After(time.Duration(5) * time.Second):
+				check(m, db)
+			case <-signalChan:
+				fmt.Println("stopping...")
+				goto stop
+			}
+		}
+	stop:*/
+}
+
+func check(m *podule.Monitor, db *bolt.DB) {
+	log.Println("Checking...")
+	counter, err := m.Now()
+
+	if err != nil {
+		log.Fatalln("Failed to backup: ", err)
+	}
+
+	if counter > 0 {
+		log.Printf("  Archived %d directories\n", counter)
+		/*db.View(func(tx *bolt.Tx) error {
+
+			b := tx.Bucket([]byte("paths"))
+
+			c := b.Cursor()
+
+			var path podule.Path
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				fmt.Printf("key=%v, value=%s\n", k, v)
+				if err := json.Unmarshal(v, &path); err != nil {
+					log.Println("failed to unmarshal data (skipping):", err)
+				}
+				path.Hash, _ = m.Paths[path.Path]
+				newData, err := json.Marshal(&path)
+				if err != nil {
+					log.Println("failed to marshal data (skipping):", err)
+				}
+				fmt.Println(newData)
+			}
+
+			return nil
+		})*/
+	} else {
+		log.Println("  No changes")
 	}
 }
