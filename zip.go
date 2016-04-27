ed1f801dbae1bec6df0701eb6f834c30700433f9
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-04-27 15:42:28 +0300

    adding singleton for zip struct

--- zip.go
+++ zip.go
@@ -1,4 +1,16 @@
 package podule
 
+import "sync"
+
 // Zip struct
 type Zip struct{}
+
+var instance *Zip
+var once sync.Once
+
+func GetInstance() *Zip {
+	once.Do(func() {
+		instance = &Zip{}
+	})
+	return instance
+}
